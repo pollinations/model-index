642e042db1142ffebc7e95b16a3ce1fce5c3c1b6
Niels Warncke <niels.warncke@gmail.com>
2022-06-21 13:43:18 +0200

    WIP: ci that fetches replicate models and gets their openapi.json

--- .github/workflows/get_openapi.py
+++ .github/workflows/get_openapi.py
@@ -5,18 +5,22 @@ import os
 with open("images.json", "r") as f:
     images = json.load(f)
 
+def system(cmd):
+    print(f"Running: {cmd}")
+    print("Exit status: ", os.system(cmd))
+
 
 def get_openapi(path, image):
     if "amazonaws" in image:
         return #private registry, repo ci is responsible for generting openapi.json
-    os.system(f"docker pull {image}")
-    os.system(f"docker inspect {image} > inspect.json", shell=True, )
+    system(f"docker pull {image}")
+    system(f"docker inspect {image} > inspect.json", shell=True, )
     with open("inspect.json", "r") as f:
         data = json.load(f)[0]
     os.makedirs(path)
     with open(f"{path}/openapi.json", "w") as f:
        f.write(data["ContainerConfig"]["Labels"]["org.cogmodel.openapi_schema"])
-    os.system("rm inspect.json")
+    system("rm inspect.json")
 
 
 for path, image in images.items():
