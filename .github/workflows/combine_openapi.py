8b7af80b32250cd9ed46e5c8336e96c462b5a6ad
Niels Warncke <niels.warncke@gmail.com>
2022-07-18 15:01:04 +0200

    ignore errors

--- .github/workflows/combine_openapi.py
+++ .github/workflows/combine_openapi.py
@@ -8,10 +8,12 @@ with open("images.json", "r") as f:
 
 all_images_open_api = {}
 for path, image in images.items():
-    with open(f"{path}/openapi.json", "r") as f:
-        open_api = json.load(f)
+    try:
+        with open(f"{path}/openapi.json", "r") as f:
+            open_api = json.load(f)
 
-    all_images_open_api[image] = open_api
+        all_images_open_api[image] = open_api
+    except: pass
 
 with open("images_openapi.json", "w") as f:
     json.dump(all_images_open_api, f, indent=4)
