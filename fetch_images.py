afb2cfb9344fa274a7fe0966d03c012e54759052
Niels Warncke <niels.warncke@gmail.com>
2022-08-15 13:50:10 +0200

    fetch and tag images in order to reactivate docker prune

--- fetch_images.py
+++ fetch_images.py
@@ -7,3 +7,5 @@ with open("images.json", "r") as f:
 
 for _, image in images.items():
     print(f"docker pull {image}")
+    if "@" in image:
+        print(f"docker tag {image} {image.split('@')[0]}")
