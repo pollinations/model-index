40b9999b077d57d67b6e5db2b10af5b4201d32f4
Niels Warncke <niels.warncke@gmail.com>
2022-08-16 00:52:08 +0200

    only pull images for the pollinator group of the machine

--- fetch_images.py
+++ fetch_images.py
@@ -1,11 +1,21 @@
 import json
-import sys
 
 
 with open("images.json", "r") as f:
     images = json.load(f)
 
+with open("metadata.json", "r") as f:
+    metadata = json.load(f)
+
+try:
+    with open("pollinator_group", "r") as f:
+        pollinator_group = f.read().strip()
+except:
+    pollinator_group = "T4"
+
 for _, image in images.items():
+    if pollinator_group not in metadata[image.split("@")[0]]["meta"]["pollinator_group"]:
+        continue
     print(f"docker pull {image}")
     if "@" in image:
         print(f"docker tag {image} {image.split('@')[0]}")
