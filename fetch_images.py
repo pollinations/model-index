679a43489c1bffb772c641f5c9db10a0350f59bd
Niels Warncke <niels.warncke@gmail.com>
2022-08-17 16:11:52 +0200

    make it work on google cloud instances

--- fetch_images.py
+++ fetch_images.py
@@ -12,7 +12,11 @@ try:
     with open("/home/ec2-user/pollinator_group", "r") as f:
         pollinator_group = f.read().strip()
 except:
-    pollinator_group = "T4"
+    try:
+        with open("/home/nielswarncke/pollinator_group", "r") as f:
+            pollinator_group = f.read().strip()
+    except:
+        pollinator_group = "T4"
 
 for _, image in images.items():
     if pollinator_group not in metadata[image.split("@")[0]]["meta"]["pollinator_group"]:
